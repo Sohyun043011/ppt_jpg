f7878a929155ca4571dedb4f91c1876fb0251e06
so1omon <solmin3665@gmail.com>
2022-06-15 13:37:33 +0900

    버튼 이벤트 추가

--- test.py
+++ test.py
@@ -10,10 +10,12 @@ from pptx.util import Pt
 from comtypes import client
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QPen, QColor, QBrush, QPixmap , QIcon
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 import socket
 import subprocess
 import threading
+import urllib.request
+import webbrowser
 
 form_class = uic.loadUiType("ppt_to_jpg.ui")[0] # ppt_to_jpg.ui(xml 형식)에서 레이아웃 및 텍스트 설정값 조정
 
@@ -27,6 +29,15 @@ class MyWindow(QMainWindow, form_class):
         self.setWindowTitle('화상회의실 관리 프로그램') # 프로그램 Title 설정
         self.setWindowIcon(QIcon('./wrench.png')) # 프로그램 아이콘 설정
         
+        self.chrome_path="C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s" # Chrome 설치 위치
+        
+        self.timer = QTimer(self)
+        self.timer.setInterval(6000)
+        self.timer.timeout.connect(self.ping)
+        self.timer.start()
+        
+        
+        
     def initSetting(self):
         # 서식 이미지 파일
         pixmap1 = QPixmap("./양식1.jpg")
@@ -37,14 +48,22 @@ class MyWindow(QMainWindow, form_class):
         self.image2.setPixmap(QPixmap(pixmap2))
         self.image3.setPixmap(QPixmap(pixmap3))
         
-        self.statusLabel.setText('상태표시합니다~')
+        self.statusLabel.setText('연결 없음')
         self.statusLabel.setStyleSheet("Color : blue")
         
+        # 서식 버튼 설정
         self.radioBtn_1.toggled.connect(self.onClicked)
         self.radioBtn_2.toggled.connect(self.onClicked)
         self.radioBtn_3.toggled.connect(self.onClicked)
         self.radioBtn_4.toggled.connect(self.onClicked)
         self.radioBtn_1.setChecked(True)
+        
+        # 활성화 버튼 설정
+        self.wallLinkBtn.connect(self.onWallOpenClick)
+        self.wallActivBtn.connect(self.onWallActivClick)
+        self.nameLinkBtn.connect(self.onNameOpenClick)
+        self.nameActivBtn.connect(self.onNameActivClick)
+        
         
     def onClicked(self):
         global pptx_fpath
@@ -72,9 +91,8 @@ class MyWindow(QMainWindow, form_class):
         qp.begin(self)
         #그리기 함수의 호출부분
         self.drawRectangles(qp)
-        qp.end()
-        
-        
+        qp.end()        
+    
     def drawRectangles(self,qp):
         qp.setBrush(QColor(255, 136, 79))
         qp.setPen(QPen(QColor(255, 136, 79), 3))
@@ -90,8 +108,38 @@ class MyWindow(QMainWindow, form_class):
         self.subject.clear()
         deptLabel = self.deptName.currentText()
         QMessageBox.about(self,"message","/"+deptLabel+"/"+subject)
+    
+    def ping(self, ip):
+        try:
+            urllib.request.urlopen('http://'+ip, timeout=1)
+            return True
+        except urllib.request.URLError as err:
+            return False
         
+    def update_network(self): # 스마트월 ip와 스마트명패 ip 각각의 연결성을 확인 후 상태 표시
+        wall_ip="192.168.0.60" #스마트월 ip
+        name_ip="192.168.0.103/Qname/empMain.aspx?readImage=ok" #스마트명패 ip
 
+        if self.ping(wall_ip):
+            self.statusLabel.setText('스마트월 연결 성공')
+        elif self.ping(name_ip):
+            self.statusLabel.setText('스마트명패 연결 성공')
+        else:
+            self.statusLabel.setText('연결 없음')
+            
+    def onNameActivClick(self): # 스마트명패 활성화 버튼 눌렀을 때 onclick function
+        os.startfile('enable.bat.lnk')
+        # self.timer.start()
+        
+    def onWallActivClick(self): # 스마트월 활성화 버튼 눌렀을 때 onclick function
+        os.startfile('enable.bat.lnk')
+    
+    def onNameOpenClick(self):
+        webbrowser.get(self.chrome_path).open("192.168.0.60")
+    
+    def onWallOpenClick(self):
+        webbrowser.get(self.chrome_path).open("http://192.168.0.103/Qname/empMain.aspx?readImage=ok")
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
