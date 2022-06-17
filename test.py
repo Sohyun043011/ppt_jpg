9debfc75849f27613fa7999b80d54a63b53c652c
Sohyun043011 <park.sohyun0430@gmail.com>
2022-06-17 17:49:03 +0900

    220617 comment 작성

--- test.py
+++ test.py
@@ -37,7 +37,7 @@ class MyWindow(QMainWindow, form_class):
         self.timer.timeout.connect(self.update_network)
         self.timer.start()
         
-        self.LayoutTab.setCurrentIndex(0)
+        self.LayoutTab.setCurrentIndex(0)       #layout1 기본 선택 되게 설정
        
         
         
@@ -81,27 +81,18 @@ class MyWindow(QMainWindow, form_class):
         with open("update_style", 'r') as f:
             self.setStyleSheet(f.read())    
         
+    #  라디오버튼을 통해 서식을 결정한 경우, 해당 서식 파일의 경로를 설정
     def onClicked(self):
         global pptx_fpath
-        global ex_flag
         radioBtn = self.sender()
         if radioBtn.text() == '서식1':
             pptx_fpath = os.path.dirname(os.path.abspath('양식1.pptx'))+'\\양식1.pptx'
-            ex_flag = '양식1'
-            # print('양식1 select' + pptx_fpath)
         elif radioBtn.text() == '서식2':
             pptx_fpath = os.path.dirname(os.path.abspath('양식2.pptx'))+'\\양식2.pptx'
-            ex_flag = '양식2'
-            # print('양식2 select'+pptx_fpath)
         elif radioBtn.text() == '서식3':
             pptx_fpath =  os.path.dirname(os.path.abspath('양식3.pptx'))+'\\양식3.pptx'
-            ex_flag = '양식3'
-            # print('양식3 select'+pptx_fpath)
-        else:
-            # pptx_fpath =  os.path.dirname(os.path.abspath('양식4.pptx'))+'\\양식4.pptx'
-            ex_flag = '그 외'
-            print('그 외 select')
             
+    #  회의실 책상 나타낸 도형 그리기 함수
     def paintEvent(self,event):
         qp = QPainter()
         qp.begin(self)
@@ -109,6 +100,7 @@ class MyWindow(QMainWindow, form_class):
         self.drawRectangles(qp)
         qp.end()
         
+    # 회의실 책상 나타낸 도형 그리기 함수
     def drawRectangles(self,qp):
         qp.setBrush(QColor(255, 136, 79))
         qp.setPen(QPen(QColor(255, 136, 79), 3))
@@ -117,19 +109,18 @@ class MyWindow(QMainWindow, form_class):
         qp.drawRect(720, 265, 30, 361)
         qp.drawRect(270, 230, 20, 191)
         
+    # 사용자가 선택한 양식을 가져와서 ppt 생성하는 함수
     def makePPT(self,directory,subject,pptx_fpath,inputValue):
         prs = Presentation(pptx_fpath)                  # 양식 선택시 불러옴
-        #슬라이드 1~15 까지 돌면서, inputValue로 부터 받아온 값들을 제목, 부제목에 넣어줌(15는 빈 슬라이드)
-        #placeholder : Title, Center Title, Subtitle, Body etc
-        
-
+        #슬라이드 1~15 까지 돌면서,기재된 값(이름,직위)을 text_on_shape() 에 넘겨줌.
         for i in range(15):
             slide = prs.slides[i]
             inputVal = inputValue[i+1]
             shapes = slide.shapes
             self.text_on_shape(shapes,inputVal)
-        prs.save(directory+'\\'+subject+'.pptx')
-        
+        prs.save(directory+'\\'+subject+'.pptx')            # /팀명/subject이름/ 폴더에 만든 ppt 저장.
+    
+    # makePPT로 만든 ppt 를 JPG로 변환해주는 함수 .1~15개 슬라이드를 1~30 장으로 변환한다. 
     def makeJPG(self,directory,subject):
         ppt = client.CreateObject('Powerpoint.Application')
         ppt.Presentations.Open(directory+"\\"+subject+".pptx")
@@ -137,20 +128,23 @@ class MyWindow(QMainWindow, form_class):
         ppt.ActivePresentation.Close()
         ppt.Quit()
         
-        # 슬라이드1~15까지 존재.->1.jpg, 2.jpg,...15.jpg 이거를 복사
+        # 슬라이드 1~15를 돌면서, 각각 1->16, 2->17,... 로 복사해준다.JPG파일 이름 또한 `슬라이드1.jpg` 를 `1.jpg` 로 변경함.
         for i in range(15):
             os.rename(directory+"\\슬라이드" + str(i+1) + ".JPG", directory + "\\" + str((i+1)) + '.jpg')
             shutil.copyfile(directory+"\\"+str((i+1)) + '.jpg',directory+"\\"+str(15+(i+1)) + '.jpg')
-        
+    
+    # 사용자가 [이름, 직위] 입력 박스에 적은 값을 받아오는 함수  
     def inputValue(self):
         inputValue= {}
+        # layout1 인 경우(이름1개, 직위1개)
         if currentIndex==0:
             for i in range(1,15):
                 nameChild = self.findChild(QLineEdit,"InputName_%d" % (i)).text()
                 namePos = self.findChild(QLineEdit,"InputPos_%d" % (i)).text()
                 inputValue[i]=[nameChild,namePos]
-            inputValue[15]=['','']
+            inputValue[15]=['','']    #(15번째 슬라이드는 항상 공란이므로, 공백으로 저장함.)
         else:
+            # layout2 인 경우(이름2개, 직위2개)
             for i in range(1,15):
                 nameChild_L = self.findChild(QLineEdit,"InputName_%d_L" % (i)).text()
                 nameChild_R = self.findChild(QLineEdit,"InputName_%d_R" % (i)).text()
@@ -160,63 +154,58 @@ class MyWindow(QMainWindow, form_class):
             inputValue[15]=[['',''],['','']]
         return inputValue
     
-    
+    # [서식 찾기] 버튼 누를 때 실행되는 함수.파일 탐색기를 열어 사용자가 선택한 양식의 path를 가져옴.
     def onClickSelect(self):
-        # QMessageBox.about(self,"message",'select vox')
-        # 서식 찾기 -> 파일 탐색기 열기-> 파일 선택-> 그 파일 경로 : pptx_fpath로 설정
-        # select_folder = QFileDialog.getExistingDirectory(self, '폴더를 선택해주세요', dataImage_default_path, QFileDialog.ShowDirsOnly)
         global pptx_fpath
-        select_file = QFileDialog.getOpenFileName(self) 
-        pptx_fpath = select_file[0]
+        select_file = QFileDialog.getOpenFileName(self)             # 파일탐색기 열림
+        pptx_fpath = select_file[0]                                 # 해당 파일의 경로(ex) C://어쩌고~//양식7.pptx
         if pptx_fpath=='':
             QMessageBox.about(self,"message","파일이 선택되지 않았습니다.다시 선택해주세요.")
-        else: self.findFormLabel.setText("선택 서식 : \n"+pptx_fpath)
+        else: self.findFormLabel.setText("선택 서식 : \n"+pptx_fpath)       #파일 제대로 선택된 경우 버튼 밑에 선택 서식 경로 표출해줌
     
+    # 사용자가 입력한 값(이름,직위)을 새로 생성한 ppt에 적용하는 함수
     def text_on_shape(self,shapes,inputVal):
-        # shapes : 한 슬라이드 안
+        # shapes : 한 슬라이드 안의 구조들
         for shape in shapes:
+            # layout1 인 경우(name,pos)
             if shape.name=="name" or shape.name=="pos":
                 # shape 내 텍스트 프레임 선택하기 & 기존 값 삭제하기
                 text_frame = shape.text_frame
                 p = text_frame.paragraphs[0]
-                font_size = p.runs[0].font.size.pt
-                font_color = p.runs[0].font.color
-                font_bold = p.runs[0].font.bold
-                font_name = p.runs[0].font.name
-                # font_brightness =  p.runs[0].font.brightness
-                # print(font_color)
-                # print(font_color.brightness)
-                text_frame.clear()
+                font_size = p.runs[0].font.size.pt      # 선택한 양식 텍스트상자의 폰트 사이즈
+                font_color = p.runs[0].font.color       # 선택한 양식 텍스트상자의 폰트 컬러
+                font_bold = p.runs[0].font.bold         # 선택한 양식 텍스트상자의 폰트 굵기
+                font_name = p.runs[0].font.name         # 선택한 양식 텍스트상자의 폰트 이름
+                text_frame.clear()                      # 폰트 설정 다 받아온 뒤, 텍스트 상자 비워줌
                 # 정렬 설정 : 중간정렬
                 p.alighnment = PP_ALIGN.CENTER   
                 run = p.add_run()
-                run.text = inputVal[0] if shape.name=="name" else inputVal[1]
+                run.text = inputVal[0] if shape.name=="name" else inputVal[1]       #이름이면 inputVal 배열의 첫번째 인자를 넣고, pos 이면 배열의 두번째 인자를 넣음
+                # 위에서 저장해 둔 폰트 설정을 그대로 적용함.
                 font = run.font 
                 font.name = font_name
-                
                 font.size = Pt(font_size)
                 if font_bold==True:
                     # bold 설정 되어있다면
                     font.bold = font_bold
+            
                 if font_color.type!=None:
-                    # 블랙아닌 경우
-                    # SCHEME인 경우
-                    
+                    # 블랙아닌 경우 - SCHEME,RGB
+                    # 1. type==SCHEME인 경우
                     if font_color.theme_color!=0:
                         font.color.theme_color=font_color.theme_color
-                        font.color.brightness = font_color.brightness
+                        font.color.brightness = font_color.brightness           #폰트 색상의 투명도가 있는 경우, 투명도 설정
                         
                     else:
-                        # RGB인 경우
-                        # print(int(f'{font_color.rgb}',16))
-                        # print(int(str(font_color.rgb)[0:2],16))
-                        
+                        # 2. type==RGB인 경우
+                        # font_color.rgb 는 FF0000 처럼 출력 되는데, 이를 255,0,0 처럼 변환함. RGBColor(255,0,0)
                         font.color.rgb = RGBColor(int(str(font_color.rgb)[0:2],16),int(str(font_color.rgb)[2:4],16),int(str(font_color.rgb)[4:6],16))
                         font.color.brightness = font_color.brightness
                 else:
-                    # 블랙인 경우,
+                    # 블랙인 경우,type이 None임.
                     font.color.rgb = RGBColor(0,0,0)
                     
+            # layout2 인 경우(name1,name2,pos1,pos2)
             elif shape.name=='name1' or shape.name=='name2' or shape.name=='pos1' or shape.name=='pos2':
                 # shape 내 텍스트 프레임 선택하기 & 기존 값 삭제하기
                 text_frame = shape.text_frame
@@ -225,9 +214,6 @@ class MyWindow(QMainWindow, form_class):
                 font_color = p.runs[0].font.color
                 font_bold = p.runs[0].font.bold
                 font_name = p.runs[0].font.name
-                # font_brightness =  p.runs[0].font.brightness
-                # print(font_color)
-                # print(font_color.brightness)
                 text_frame.clear()
                 # 정렬 설정 : 중간정렬
                 p.alighnment = PP_ALIGN.CENTER   
@@ -243,24 +229,18 @@ class MyWindow(QMainWindow, form_class):
                     
                 font = run.font 
                 font.name = font_name
-                
                 font.size = Pt(font_size)
                 if font_bold==True:
                     # bold 설정 되어있다면
                     font.bold = font_bold
                 if font_color.type!=None:
-                    # 블랙아닌 경우
-                    # SCHEME인 경우
-                    
+                    # 블랙아닌 경우 - SCHEME,RGB
+                    # 1. type==SCHEME인 경우
                     if font_color.theme_color!=0:
                         font.color.theme_color=font_color.theme_color
                         font.color.brightness = font_color.brightness
-                        
                     else:
-                        # RGB인 경우
-                        # print(int(f'{font_color.rgb}',16))
-                        # print(int(str(font_color.rgb)[0:2],16))
-                        
+                        # 2. type==RGB인 경우
                         font.color.rgb = RGBColor(int(str(font_color.rgb)[0:2],16),int(str(font_color.rgb)[2:4],16),int(str(font_color.rgb)[4:6],16))
                         font.color.brightness = font_color.brightness
                 else:
@@ -273,31 +253,34 @@ class MyWindow(QMainWindow, form_class):
             self.wallLinkBtn.setDisabled(True) # 초기 링크 버튼 비활성화
             func(self)
         return wrapper            
-                
+    
+    # create 버튼 클릭시 이벤트       
     def createBtn_clicked(self):
-        # create 버튼 클릭시 이벤트
-        # 현재 tab이 어디인지 확인(0 or 1)
+        # 현재 tab이 어디인지 확인 (layout1 : 0 or layout2 : 1)
         global currentIndex
         currentIndex = self.LayoutTab.currentIndex()
         
         # /팀이름/subject/ 로 폴더 생성
-        subject = self.subject.text() if currentIndex==0 else self.subject_2.text() # 폴더 이름
+        subject = self.subject.text() if currentIndex==0 else self.subject_2.text()         # 폴더 이름
+        
         if subject=='':
-            # 공백인 경우 입력하게 하기
+            # 공백인 경우 alert 띄움
             QMessageBox.about(self,"message","폴더명을 입력해주세요.")
         else:
-            deptLabel = self.deptName.currentText() if currentIndex==0 else self.deptName_2.currentText()                # 부서명
+            deptLabel = self.deptName.currentText() if currentIndex==0 else self.deptName_2.currentText()   # 부서명
             # directory = dataImage_default_path+"\\"+deptLabel+"\\"+subject     # 디렉토리 경로
             directory = os.getcwd()+"\\"+deptLabel+"\\"+subject
-            inputValue = self.inputValue()     
+            inputValue = self.inputValue()      #사용자가 입력한 정보
             if not os.path.exists(directory):
                 os.makedirs(directory)
                 # 폴더 생성 후, ppt 생성
-                self.makePPT(directory,subject,pptx_fpath,inputValue)       #디렉토리 경로,선택한 양식경로, 입력값
-                self.makeJPG(directory,subject) 
+                self.makePPT(directory,subject,pptx_fpath,inputValue)       #디렉토리 경로,폴더이름,선택한 양식경로,입력값
+                self.makeJPG(directory,subject)                             #디렉토리 경로, 폴더이름
             else: 
-                # 이미 있는 폴더인 경우, 이름 다시 설정.
+                # 이미 있는 폴더인 경우, alert 띄움
                 QMessageBox.about(self,"message",subject+"는 이미 있는 폴더입니다. 다른 이름을 설정해주세요.")
+                
+            # create 버튼실행이 완료된 경우, subject 입력박스와 선택 서식 표출 박스 비워줌.
             self.subject.clear()
             self.subject_2.clear()
             self.findFormLabel.clear()
